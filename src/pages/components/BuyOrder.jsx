b721d9bf89b7980f88f8cee9b257cff2cbf1fee5
Garrett Heiner <64627881+Heiner000@users.noreply.github.com>
2023-05-22 16:38:16 -0700

    adds more structure to buy order modal

--- src/pages/components/BuyOrder.jsx
+++ src/pages/components/BuyOrder.jsx
@@ -1,7 +1,17 @@
-export default function BuyOrder(){
+export default function BuyOrder(props){
     return(
         <div>
-            <h1>buy order</h1>
+            <h1>BUY ORDER</h1>
+
+            <h2>Price per share:</h2>
+            <p>{companyData.price}</p>
+
+            <p>How many shares?</p>
+            <div className="">
+                <button>-</button>
+                <input type="integer" id="quantity" />
+                <button>+</button>
+            </div>
         </div>
     )
 }
