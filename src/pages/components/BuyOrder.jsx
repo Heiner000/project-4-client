e40b921f91133056bc3099279f64074414d55010
Garrett Heiner <64627881+Heiner000@users.noreply.github.com>
2023-05-22 16:55:21 -0700

    stubs out buy order, needs logic

--- src/pages/components/BuyOrder.jsx
+++ src/pages/components/BuyOrder.jsx
@@ -1,10 +1,13 @@
-export default function BuyOrder(props){
-    return(
+export default function BuyOrder(props) {
+
+    // total_cost = {props.price} * {quantity}
+
+    return (
         <div>
             <h1>BUY ORDER</h1>
 
             <h2>Price per share:</h2>
-            <p>{companyData.price}</p>
+            <p>$$$</p>
 
             <p>How many shares?</p>
             <div className="">
@@ -12,6 +15,18 @@ export default function BuyOrder(props){
                 <input type="integer" id="quantity" />
                 <button>+</button>
             </div>
+
+            <h3>Total Cost:</h3>
+            <p>$$$</p>
+
+            <h4>Funds Available:</h4>
+            <p>$$$</p>
+
+            <div className="">
+                <button>BUY NOW</button>
+                <button>Cancel</button>
+            </div>
+
         </div>
     )
 }
