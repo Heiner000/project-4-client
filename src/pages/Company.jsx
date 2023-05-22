879e3fe49b82547963b14759d6b170f0718dbf09
Garrett Heiner <64627881+Heiner000@users.noreply.github.com>
2023-05-22 13:27:21 -0700

    adds useEffect to fetch companyData, not currently functioning

--- src/pages/Company.jsx
+++ src/pages/Company.jsx
@@ -1,7 +1,27 @@
-export default function Company(){
+import React, {useState, useEffect } from 'react'
+import axios from 'axios'
+
+export default function Company({ ticker }){
+    const [companyData, setCompanyData] = useState({ name: '', price: ''})
+
+    useEffect(() => {
+        const fetchCompanyData = async () => {
+            try {
+                const response = await axios.get(`http://localhost:8000/assets/search/${ticker}/`)
+                setCompanyData({ name: response.data[0], price: response.data[1] })
+                console.log(response.data)
+            } catch (err) {
+                console.log('Error fetching company data: ', err)
+            }
+        }
+        fetchCompanyData()
+    }, [ticker])
+
     return(
         <div>
-            <h1>company page</h1>
+            <h1>{companyData.name}</h1>
+            
+            <p>current price: {companyData.price}</p>
         </div>
     )
 }
