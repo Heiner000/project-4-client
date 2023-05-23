de3bf3383ba5cffadc9bf7ec4c249d1a4b0bd835
brogers-97 <brburr24@gmail.com>
2023-05-23 00:10:13 -0700

    added watchlist function and style

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,24 +1,88 @@
 import axios from 'axios'
-import { useState,useEffect } from 'react'
+import ReactSelect from 'react-select'
+import { useState, useEffect } from 'react'
 import './styles/homepage.css'
 
 export default function HomePage(){
 
-  const [stocks, setStocks] = useState()
+  const [selectedStock, setSelectedStock] = useState()
+  const [watchlist, setWatchlist] = useState()
+  const options = [
+    { value: 'aapl', label: 'Apple - AAPL' },
+    { value: 'msft', label: 'Microsoft - MSFT' },
+    { value: 'amzn', label: 'Amazon - AMZN' },
+    { value: 'goog', label: 'Google - GOOG' },
+    { value: 'fb', label: 'Facebook - FB' },
+    { value: 'tsla', label: 'Tesla - TSLA' },
+    { value: 'brk.b', label: 'Berkshire Hathaway - BRK.B' },
+    { value: 'v', label: 'Visa - V' },
+    { value: 'jnj', label: 'Johnson & Johnson - JNJ' },
+    { value: 'wmt', label: 'Walmart - WMT' },
+    { value: 'jpm', label: 'JPMorgan Chase - JPM' },
+    { value: 'pg', label: 'Procter & Gamble - PG' },
+    { value: 'xom', label: 'Exxon Mobil - XOM' },
+    { value: 'ba', label: 'Boeing - BA' },
+    { value: 'baba', label: 'Alibaba - BABA' },
+    { value: 'dis', label: 'Disney - DIS' },
+    { value: 'nvda', label: 'NVIDIA - NVDA' },
+    { value: 'nflx', label: 'Netflix - NFLX' },
+    { value: 'hd', label: 'Home Depot - HD' },
+    { value: 'ko', label: 'Coca Cola - KO' },
+    { value: 'ma', label: 'Mastercard - MA' },
+    { value: 'intc', label: 'Intel - INTC' },
+    { value: 'vz', label: 'Verizon - VZ' },
+    { value: 'unh', label: 'UnitedHealth Group - UNH' },
+    { value: 'pfe', label: 'Pfizer - PFE' },
+    { value: 'mrk', label: 'Merck - MRK' },
+    { value: 'pep', label: 'PepsiCo - PEP' },
+    { value: 'cvs', label: 'CVS Health - CVS' },
+    { value: 'cmcsa', label: 'Comcast - CMCSA' },
+    { value: 'bac', label: 'Bank of America - BAC' },
+    { value: 'adbe', label: 'Adobe - ADBE' },
+    { value: 'c', label: 'Citigroup - C' },
+    { value: 'orcl', label: 'Oracle - ORCL' },
+    { value: 'pypl', label: 'Paypal - PYPL' },
+    { value: 'ibm', label: 'IBM - IBM' },
+    { value: 'mcd', label: 'McDonald\'s - MCD' },
+    { value: 't', label: 'AT&T - T' },
+    { value: 'abbv', label: 'AbbVie - ABBV' },
+    { value: 'acn', label: 'Accenture - ACN' },
+    { value: 'abt', label: 'Abbott Laboratories - ABT' },
+    { value: 'crm', label: 'Salesforce - CRM' },
+  ];
+  
+
+
+  useEffect(() => {
+    const user_id = 3
+    const getWatchlist = async () => {
+      try{
+        const response = await axios.get('http://localhost:8000/get_watchlist/', {params: {user_id: user_id}})
+        setWatchlist(response.data)
+      }catch(err){
+        console.log(err)
+      }
+    }
+    getWatchlist()
+  }, [])
+
+
+  const handleChange = (selectedOption) => {
+    setSelectedStock(selectedOption);
+  };
+
+
 
   const addStock = async () => {
     const data = {
-      asset_type: 'stock',
-      ticker: 'aapl',
-      quantity: 5,
-      price: 81.25,
-      trade_type: 'buy',
-      user_id: 3
+      user_id: 3, // this will change accordignly to the logged in user
+      new_stock: selectedStock.value
     }
     try {
-      const response = await axios.post('http://localhost:8000/trades/', data)
+      const response = await axios.post('http://localhost:8000/watchlist/', data)
       if (response.status === 201){
         console.log("Trade created successfully")
+        getWatchlist()
       } else {
         console.log("Unable to create trade")
       }
@@ -26,6 +90,37 @@ export default function HomePage(){
       console.log(err)
     }
   }
+
+  const getWatchlist = async () => {
+    try{
+      const response = await axios.get('http://localhost:8000/get_watchlist/', {params: {user_id: 3}})
+      setWatchlist(response.data)
+    }catch(err){
+      console.log(err)
+    }
+  }
+
+
+  // const addStock = async () => {
+  //   const data = {
+  //     asset_type: 'stock',
+  //     ticker: 'aapl',
+  //     quantity: 5,
+  //     price: 81.25,
+  //     trade_type: 'buy',
+  //     user_id: 3
+  //   }
+  //   try {
+  //     const response = await axios.post('http://localhost:8000/trades/', data)
+  //     if (response.status === 201){
+  //       console.log("Trade created successfully")
+  //     } else {
+  //       console.log("Unable to create trade")
+  //     }
+  //   } catch (err) {
+  //     console.log(err)
+  //   }
+  // }
 
 
   useEffect( () => {
@@ -49,34 +144,63 @@ export default function HomePage(){
   //   }
   // }
 
+  const displayWatchlist = () => {
+    return [...watchlist].reverse().map((ticker, i) => {
+      return(
+        <div className='watch-stock'>
+          <p key={i}>{ticker}</p>
+        </div>
+      )
+    })
+  }
+
+
+
+
   const displayStocks = async () => {
     axios.get(`http://localhost:8000/portfolio/`)
   }
 
+
+
+
     return(
         <div className='container'>
             <h1>Hi, Brian</h1>
-            <h2>portfolio</h2>
-            <div className='portfolio-container'>
-              <p>add stock</p>
-              <select name="" id="">
-                <option value="aapl">Apple - aapl</option>
-                <option value="aapl">Apple - AAPL</option>
-                <option value="googl">Google - GOOGL</option>
-                <option value="msft">Microsoft - MSFT</option>
-                <option value="amzn">Amazon - AMZN</option>
-                <option value="fb">Facebook - FB</option>
-                <option value="tsla">Tesla - TSLA</option>
-                <option value="nflx">Netflix - NFLX</option>
-                <option value="dis">Disney - DIS</option>
-                <option value="baba">Alibaba - BABA</option>
-                <option value="v">Visa - V</option>
-                <option value="ma">Mastercard - MA</option>
-              </select>
-              <button onClick={addStock}>add +</button>
+            <div className='outer-portfolio'>
+            <h2>Portfolio</h2>
+              <div className='portfolio-container'>
+                {/* <ReactSelect
+                  value={selectedStock}
+                  onChange={handleChange}
+                  options={options}
+                  />
+                <button onClick={addStock}>add +</button> */}
+              </div>
             </div>
-            <div>
+
+            <div className='outer-watchlist'>
               <h2>Watchlist</h2>
+              <div className='watchlist-container'>
+                {watchlist ? displayWatchlist() : 'loading'}
+              </div>
+
+              <div className='new-stock'>
+                <ReactSelect className='select'
+                  value={selectedStock}
+                  onChange={handleChange}
+                  options={options}
+                  styles={{
+                    control: (provided) => ({
+                      ...provided,
+                      backgroundColor: 'transparent',
+                      border: '2px solid #0066cc'
+                    }),
+                  }}
+                  />
+                <button onClick={addStock}>+</button>
+              </div>
+
             </div>
         </div>
     )
