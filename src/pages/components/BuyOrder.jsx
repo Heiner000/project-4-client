c8d0a1fff40408cb27e8113f7300af2b6dd1e669
Garrett Heiner <64627881+Heiner000@users.noreply.github.com>
2023-05-22 17:47:24 -0700

    adds logic to buy and sell, needs more

--- src/pages/components/BuyOrder.jsx
+++ src/pages/components/BuyOrder.jsx
@@ -14,7 +14,9 @@ export default function BuyOrder(props) {
     }
 
     const calculateTotalPrice = () => {
-        return props.price * quantity
+        let price = parseFloat(props.companyData.price)
+        let totalPrice = price * quantity
+        return totalPrice.toFixed(2)
     }
 
     return (
@@ -22,30 +24,30 @@ export default function BuyOrder(props) {
             <h1>BUY ORDER</h1>
 
             <h2>Price per share:</h2>
-            <p>{props.price}</p>
+            <p>{props.companyData.price}</p>
 
             <p>How many shares?</p>
             <div className="">
                 <button onClick={handleDecrement}>-</button>
-                <input 
-                    type="number" 
-                    id="buy-quantity" 
+                <input
+                    type="number"
+                    id="buy-quantity"
                     value={quantity}
-                onChange={(e) => setQuantity(parseInt(e.target.value))}
+                    onChange={(e) => setQuantity(parseInt(e.target.value))}
                 />
                 <button onClick={handleIncrement}>+</button>
             </div>
 
             <h3>Total Cost:</h3>
-            <p>{calculateTotalPrice}</p>
+            <p>{calculateTotalPrice()}</p>
 
             {/* need to pull funds available from user model */}
             <h4>Funds Available:</h4>
-            <p>$$$</p> 
+            <p>$ 10,000</p>
 
             <div className="">
                 <button>BUY NOW</button>
-                <button>Cancel</button>
+                <button onClick={() => props.closeModal()}>Cancel</button>
             </div>
 
         </div>
--- src/pages/components/SellOrder.jsx
+++ src/pages/components/SellOrder.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
 
 export default function SellOrder(props){
-    const [quantity, setQuantity] = useState(0)
+    const [quantity, setQuantity] = useState(1)
     // INCOMPLETE: Need to fetch user's shares
     const [userShares, setUserShares] = useState(0)
 
     const handleDecrement = () => {
-        if (quantity > 0) {
+        if (quantity > 1) {
             setQuantity(quantity - 1)
         }
     }
@@ -18,7 +18,9 @@ export default function SellOrder(props){
     }
 
     const calculateTotalPrice = () => {
-        return props.price * quantity
+        let price = parseFloat(props.companyData.price)
+        let totalPrice = price * quantity
+        return totalPrice.toFixed(2)
     }
 
     return(
@@ -26,25 +28,30 @@ export default function SellOrder(props){
             <h1>SELL ORDER</h1>
 
             <h2>Price per share:</h2>
-            <p>{props.price}</p>
+            <p>{props.companyData.price}</p>
 
             <p>How many shares?</p>
             <div className="">
                 <button onClick={handleDecrement}>-</button>
-                <input type="integer" id="sell-quantity" />
+                <input 
+                    type="number" 
+                    id="sell-quantity" 
+                    value={quantity}
+                    onChange={(e) => setQuantity(parseInt(e.target.value))}
+                />
                 <button onClick={handleIncrement}>+</button>
             </div>
 
             <h3>Sale Total:</h3>
-            <p>{calculateTotalPrice}</p>
+            <p>{calculateTotalPrice()}</p>
 
             {/* need to pull funds from user model */}
             <h4>Funds After Sale:</h4>
-            <p>$$$</p>
+            <p>$$$ + {calculateTotalPrice()}</p>
 
             <div className="">
                 <button>SELL NOW</button>
-                <button>Cancel</button>
+                <button onClick={() => props.closeModal()}>Cancel</button>
             </div>
 
         </div>
