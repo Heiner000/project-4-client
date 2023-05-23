34651a50853f74b8811a46f8ff27c582873dc4a6
brogers-97 <brburr24@gmail.com>
2023-05-23 15:48:20 -0700

    profile functions

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,16 @@
+import './styles/profile.css'
+import { useState, useEffect } from 'react'
+
 export default function Profile(){
+
+    const [zipCode, setZipCode] = useState()
+
+
+    useEffect()
+
     return(
-        <div>
+        <div className='profile-container'>
+            <img className="profile-page-logo" src="../images/profile-logo.png" alt="profile logo" />
             <h1>profile page</h1>
         </div>
     )
