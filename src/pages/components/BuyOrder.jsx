5a71073b796c95cd70e59cf241c4d9023959c862
Garrett Heiner <64627881+Heiner000@users.noreply.github.com>
2023-05-22 17:18:52 -0700

    stubs out sell order, needs props

--- src/pages/components/BuyOrder.jsx
+++ src/pages/components/BuyOrder.jsx
@@ -1,26 +1,47 @@
-export default function BuyOrder(props) {
+import { useState } from 'react'
 
-    // total_cost = {props.price} * {quantity}
+export default function BuyOrder(props) {
+    const [quantity, setQuantity] = useState(0)
+
+    const handleDecrement = () => {
+        if (quantity > 0) {
+            setQuantity(quantity - 1)
+        }
+    }
+
+    const handleIncrement = () => {
+        setQuantity(quantity + 1)
+    }
+
+    const calculateTotalPrice = () => {
+        return props.price * quantity
+    }
 
     return (
         <div>
             <h1>BUY ORDER</h1>
 
             <h2>Price per share:</h2>
-            <p>$$$</p>
+            <p>{props.price}</p>
 
             <p>How many shares?</p>
             <div className="">
-                <button>-</button>
-                <input type="integer" id="quantity" />
-                <button>+</button>
+                <button onClick={handleDecrement}>-</button>
+                <input 
+                    type="number" 
+                    id="buy-quantity" 
+                    value={quantity}
+                onChange={(e) => setQuantity(parseInt(e.target.value))}
+                />
+                <button onClick={handleIncrement}>+</button>
             </div>
 
             <h3>Total Cost:</h3>
-            <p>$$$</p>
+            <p>{calculateTotalPrice}</p>
 
+            {/* need to pull funds available from user model */}
             <h4>Funds Available:</h4>
-            <p>$$$</p>
+            <p>$$$</p> 
 
             <div className="">
                 <button>BUY NOW</button>
--- src/pages/components/SellOrder.jsx
+++ src/pages/components/SellOrder.jsx
@@ -1,7 +1,52 @@
-export default function SellOrder(){
+import { useState } from 'react'
+
+export default function SellOrder(props){
+    const [quantity, setQuantity] = useState(0)
+    // INCOMPLETE: Need to fetch user's shares
+    const [userShares, setUserShares] = useState(0)
+
+    const handleDecrement = () => {
+        if (quantity > 0) {
+            setQuantity(quantity - 1)
+        }
+    }
+
+    const handleIncrement = () => {
+        if (quantity < userShares){
+            setQuantity(quantity + 1)
+        }
+    }
+
+    const calculateTotalPrice = () => {
+        return props.price * quantity
+    }
+
     return(
         <div>
-            <h1>sell order</h1>
+            <h1>SELL ORDER</h1>
+
+            <h2>Price per share:</h2>
+            <p>{props.price}</p>
+
+            <p>How many shares?</p>
+            <div className="">
+                <button onClick={handleDecrement}>-</button>
+                <input type="integer" id="sell-quantity" />
+                <button onClick={handleIncrement}>+</button>
+            </div>
+
+            <h3>Sale Total:</h3>
+            <p>{calculateTotalPrice}</p>
+
+            {/* need to pull funds from user model */}
+            <h4>Funds After Sale:</h4>
+            <p>$$$</p>
+
+            <div className="">
+                <button>SELL NOW</button>
+                <button>Cancel</button>
+            </div>
+
         </div>
     )
 }
